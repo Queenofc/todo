04678779cb0082c278e325ad7113d3441cd2658f
Queenofc <152055731+Queenofc@users.noreply.github.com>
2025-02-03 19:34:23 +0530

    finished

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +26,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>By {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
