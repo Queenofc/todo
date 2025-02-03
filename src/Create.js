f258f9e801c8f2beafcea0e8c5ed935a13dfb32d
Queenofc <152055731+Queenofc@users.noreply.github.com>
2025-02-03 19:37:39 +0530

    finished

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Create = () => {
   const [title, setTitle] = useState(" ");
   const [body, setBody] = useState(" ");
-  const [author, setAuthor] = useState("mario");
+  const [author, setAuthor] = useState(" ");
   const [isPending, setIsPending] = useState(false);
   const navigate = useNavigate();
 
@@ -19,7 +19,7 @@ const Create = () => {
     }).then(() => {
       console.log("new blog added");
       setIsPending(false);
-      navigate('/')
+      navigate("/");
     });
   };
 
@@ -41,10 +41,12 @@ const Create = () => {
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author:</label>
-        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
-        </select>
+        <input
+          type="text"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
         {!isPending && <button>Add blog</button>}
         {isPending && <button disabled>Adding blog...</button>}
       </form>
